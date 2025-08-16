99a30f31863f83f1f35067633f6fa3feb9216ec6
mushax <bitfares@gmail.com>
2025-08-17 02:13:41 +0300

    Fix: Finalize all feature tests for multi-language support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // This line tells all tests to ignore Vite
+        $this->withoutVite();
+    }
 }
